bd61811a7390072c58ae7e0b4561fcc583fab311
anjali-choudhary1130 <anjalijaiswalmuz1@gmail.com>
2026-02-08 13:26:09 +0530

    force backend url

--- QuickChat/frontend/src/lib/axios.js
+++ QuickChat/frontend/src/lib/axios.js
@@ -1,14 +1,37 @@
 import axios from "axios"
 
-const API_URL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:5000/api"
-    : "https://quickchat-backend-osnh.onrender.com/api"
-
 export const axiosInstance = axios.create({
-  baseURL: API_URL,
+  baseURL: "https://quickchat-backend-osnh.onrender.com/api",
   withCredentials: true,
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import axios from "axios"
+
+// const API_URL =
+//   import.meta.env.MODE === "development"
+//     ? "http://localhost:5000/api"
+//     : "https://quickchat-backend-osnh.onrender.com/api"
+
+// export const axiosInstance = axios.create({
+//   baseURL: API_URL,
+//   withCredentials: true,
+// })
 
 
 
